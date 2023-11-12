cef27a43d6aecce7d50e351ab2250584575df3df
MSK1206 <phantom7216@yahoo.co.jp>
2023-11-12 22:23:55 +0900

    Add: PrismaAdapter, NEXTAUTH_SECRETの追加

--- auth.ts
+++ auth.ts
@@ -1,10 +1,14 @@
 import NextAuth from 'next-auth';
 import GitHub from '@auth/core/providers/github';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/lib/prisma';
 
 export const {
   handlers: { GET, POST },
   auth,
 } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET as string,
   providers: [
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
